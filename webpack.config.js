313cc779332f78bcec0f13f17bac0c623a8fff59
simon louvet <simon.louvet.zen@gmail.com>
2018-03-22 15:47:21 +0100

    npm indus

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = [{
   entry: {
-    front: './src/SemViz.js',
+    front: './src/testComponent/external.js',
   },
   output: {
     filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public/testComponent')
   },
   module: {
     rules: [{
@@ -20,6 +20,28 @@ module.exports = [{
     }]
   },
   plugins: [new HtmlWebpackPlugin({
-    template: 'src/index.html'
+    template: 'src/testComponent/testComponent.html'
+  })]
+},{
+  entry: {
+    front: './src/testComponentContainer/external.js',
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'public/testComponentContainer')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {}
+      }
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject : false,
+    template: 'src/testComponentContainer/testComponentContainer.html',
   })]
 }];
